3a0cf4c0d761caf82b573baf8f1a102884bfef4f
ibrahimgamill <121096964+ibrahimgamill@users.noreply.github.com>
2025-06-25 00:05:43 +0300

    updated

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -76,6 +76,11 @@ export default function ProductDetails() {
                                             fontSize: 16,
                                             transition: "border .15s, background .13s"
                                         }}
+                                        data-testid={
+                                            selected[attr.name] === item.value
+                                                ? `cart-item-attribute-${attrKebab}-${attrKebab}-selected`
+                                                : `cart-item-attribute-${attrKebab}-${attrKebab}`
+                                        }
                                     >
                                         {attr.type === "swatch"
                                             ? <span style={{
